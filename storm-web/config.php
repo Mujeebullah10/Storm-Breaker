cca9f5f27cfb23438ac2cc8c3a233603ee6d8444
Mujeebullah10 <Mujeebullah2121@gmail.com>
2025-08-29 18:05:16 +0500

    Update config.php

--- storm-web/config.php
+++ storm-web/config.php
@@ -2,8 +2,8 @@
 
 $CONFIG = array (
     "admin" => [
-        "fullname" => "hacker", 
-        "password" => "admin",
+        "fullname" => "khan", 
+        "password" => "mujeeb",
     ], 
     
 );
